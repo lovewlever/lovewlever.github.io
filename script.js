76aeddb8ce03e60d32f08c854606cad8ae7e6e37
lovewlever <35085409+lovewlever@users.noreply.github.com>
2021-09-07 18:02:11 +0800

    delete

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('click', function () {
-  document.querySelectorAll('css-doodle').forEach(function (o) {o.update();});
-});
-document.addEventListener('touchstart', function () {
-  document.querySelectorAll('css-doodle').forEach(function (o) {o.update();});
-});
